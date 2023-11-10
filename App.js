07d68bf5bf018832680b7af9b31d9027f5b277e2
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2023-11-10 15:27:55 +0000

    flex layout

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput placeholder='your goal' style={styles.textInput} />
         <Button title='add goal' />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>list of goals</Text>
       </View>
     </View>
@@ -16,18 +16,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
-    alignItems: 'stretch',
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
+    flex: 1,
     borderWidth: 1,
     borderColor: '#cccccc',
-    flex: 1,
     marginRight: 8,
     padding: 5,
+  },
+  goalsContainer: {
+    flex: 4,
   },
 })
